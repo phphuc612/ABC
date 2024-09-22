578be8f7f3ae052f49b9da27520269b8131bd79c
phphuc612 <phphuc612@gmail.com>
2024-09-22 09:42:57 +0000

    [feat] add training and logging util

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,59 +1,8 @@
-import os
-from pathlib import Path
-from typing import Literal, Optional
-
-import pydantic
-
-from configs.envs import GLOBAL_CONFIG
-
 from .image_encoder import HFImageEncoder
-
-
-class EncoderSchema(pydantic.BaseModel):
-    source: str
-    name: str
-    pretrained: bool = True
-    gradient_checkpointing: bool = False
-
-    cache_dir: Path = GLOBAL_CONFIG.cache_dir
-    local_files_only: Optional[bool] = pydantic.Field(None)
-
-    max_length: int = 256
-
-    freeze: bool = False
-
-
-class ImageEncoderSchema(EncoderSchema):
-    model_type: str = "swin"
-
-    img_size: int = 224
-    # Avoid warning
-    model_config = pydantic.ConfigDict(
-        protected_namespaces=("protect_me_", "also_protect_")
-    )
-
-    pooling: Literal["bos", "mean"] = "mean"
-
-
-def load_image_encoder(config: ImageEncoderSchema):
-    if config.source.lower() == "huggingface":
-        local_file_exists = config.local_files_only
-        if not local_file_exists:
-            local_file_exists = os.path.exists(
-                os.path.join(
-                    config.cache_dir,
-                    f'models--{config.name.replace("/", "--")}',
-                )
-            )
-        _image_encoder = HFImageEncoder(
-            **{
-                **config.dict(exclude={"source"}),
-                "local_files_only": local_file_exists,
-            }
-        )
-    else:
-        raise NotImplementedError(
-            f"Image encoder source {config.source} not implemented"
-        )
-
-    return _image_encoder
+from .projection_head import MLPProjectionHead
+from .schema import (
+    ImageEncoderSchema,
+    ProjectionHeadSchema,
+    load_image_encoder,
+    load_projection_head,
+)
